75210e58eaf0e78cf2e77f4772d10e56edda8cc2
MrMoon127 <sasha.andia@gmail.com>
2024-02-20 20:51:40 -0800

    added comments

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// connects the app and ejs to the public folder
+// now we can pull styles and scripts from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("view engine", "ejs")
