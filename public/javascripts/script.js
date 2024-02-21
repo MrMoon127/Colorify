ecc0b3ec1deb356648f01284e0cbe1766bdc2bc9
MrMoon127 <sasha.andia@gmail.com>
2024-02-20 20:57:38 -0800

    added a random button that turns circle into a random color

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,21 @@
 function paint(color) {
-  console.log(color);
-
   const circle = document.getElementById('circleID');
+  circle.style = `background-color:${color}`
+}
+
+function random() {
+  const colors = [
+    'yellow',
+    'blue',
+    'green',
+    'red',
+    'purple',
+    'white',
+    'black',
+    'orange',
+  ];
+  const circle = document.getElementById('circleID');
+
+  const color = colors[(Math.floor(Math.random() * colors.length))];
   circle.style = `background-color:${color}`
 }
